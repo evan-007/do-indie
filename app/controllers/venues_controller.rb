115616c3ce2a3a5d5083d92255e8afbd8f9e31f0
Evan Lloyd <eulloyd@gmail.com>
2014-02-28 19:53:54 +0900

    Complete create venue spec

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,5 @@
 class VenuesController < ApplicationController
-  before_filter :authenticate_user, only: [:new, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :get_venue, only: [:show, :edit, :update, :destoy]
   
   def index
@@ -8,11 +8,30 @@ class VenuesController < ApplicationController
   
   def show
   end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.create(venue_params)
+    if @venue.save
+      flash[:notice] = "Venue Created!"
+      redirect_to venue_path(@venue)
+    else
+      flash[:notice] = "Not created!"
+      redirect_to new_venue_path
+    end
+  end
   
   
   private
   
   def get_venue
     @venue = Venue.find(params[:id])
+  end
+
+  def venue_params
+    params.require(:venue).permit(:name, :phone, :address, :misc, :facebook, :cafe, :website)
   end
 end
---
+++ spec/features/creating_venues_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+feature "Creating venues" do
+	before do
+		@user = create(:admin_user)
+		sign_in @user
+	end
+	scenario "Users can create venues" do
+		visit new_venue_path
+		fill_in "Name", with: "Avalon"
+		fill_in "Phone", with: "010-1234-1234"
+		fill_in "Address", with: "Gangnam station, exit 2"
+		fill_in "Misc", with: "Best club in G-nam"
+		fill_in "Facebook", with: "facebook.com"
+		fill_in "Cafe", with: "naver.com"
+		fill_in "Website", with: "gangnamstyle.com"
+		click_button "Create Venue"
+		expect(page).to have_content("Venue Created!")
+	end
+
+	
+end
