19fb73808608e94cdfa4c4a7f14f58b79b7b41ce
Evan Lloyd <eulloyd@gmail.com>
2014-03-02 19:22:45 +0900

    Change bands pagination

--- app/models/band.rb
+++ app/models/band.rb
@@ -5,7 +5,7 @@ class Band < ActiveRecord::Base
 	has_many :events, through: :event_bands
 	validates :name, presence: true, uniqueness: true
 
-	paginates_per 100 #fix pagination
+	paginates_per 50 #fix pagination
 
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
