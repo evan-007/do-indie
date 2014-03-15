35c60620aa7d240c134ef71c6763650c9e76e9f6
Evan Lloyd <eulloyd@gmail.com>
2014-03-15 22:16:11 +0900

    Remove old twitter config

--- config/initializers/twitter_credentials.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = 'your_consumer_key_here'
-  config.consumer_secret = 'your_consumer_secret_here'
-  config.oauth_token = 'your_oauth_token_here'
-  config.oauth_token_secret = 'your_oauth_token_secret_here'
-end
