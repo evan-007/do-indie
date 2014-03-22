86738c3650bad09e016fe30739ac223abf4e3005
Evan Lloyd <eulloyd@gmail.com>
2014-03-22 14:14:53 +0900

    Complete admin cities spec

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CitiesController < ApplicationController
+	before_action :get_city, only: [:edit, :update, :destroy]
 	def index
 		@cities = City.admin_search(params[:query], params[:page])
 	end
@@ -7,12 +8,25 @@ class Admin::CitiesController < ApplicationController
 		redirect_to edit_admin_city_path(params[:id])
 	end
 
+	def new
+		@city = City.new
+	end
+
+	def create
+		@city = City.new(city_params)
+		if @city.save
+			flash[:notice] = "Successfully created the city"
+			redirect_to admin_cities_path
+		else
+			flash[:notice] = "Opps, didn't create the city"
+			redirect_to admin_cities_path
+		end
+	end
+
 	def edit
-		@city = City.find(params[:id])
 	end
 
 	def update
-		@city = City.find(params[:id])
 		if @city.update(city_params)
 			flash[:notice] = "Successfully updated the city"
 			redirect_to admin_cities_path
@@ -22,9 +36,23 @@ class Admin::CitiesController < ApplicationController
 		end
 	end
 
+	def destroy
+		if @city.destroy
+			flash[:notice] = "Boom! City deleted"
+			redirect_to admin_cities_path
+		else
+			flash[:notice] = "Whoops, can't delete that!"
+			redirect_to admin_cities_path
+		end
+	end
+
 	private
 
 	  def city_params
 	  	params.require(:city).permit(:en_name, :ko_name)
+	  end
+
+	  def get_city
+	  	@city = City.find(params[:id])
 	  end
 end
--- spec/features/admin_cities_spec.rb
+++ spec/features/admin_cities_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-feature "Editing cities" do
+feature "Admin cities panel" do
 	before do
 		@admin = create(:admin_user)
 		@city = create(:city)
@@ -12,7 +12,17 @@ feature "Editing cities" do
 		click_link "Cities"
 		click_link @city.en_name
 		fill_in "city[ko_name]", with: "Incheon"
-		click_button "Update city"
+		click_button "Update City"
 		expect(page).to have_content "updated"
+	end
+
+	scenario "Admin users can create new cities" do
+		visit admin_root_en_path
+		click_link "Cities"
+		click_link "New City"
+		fill_in "city[en_name]", with: "New York"
+		fill_in "city[ko_name]", with: "New York-uh"
+		click_button "Create City"
+		expect(page).to have_content "created"
 	end
 end
