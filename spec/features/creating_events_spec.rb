860dbddc3a080b3966012d6b08d9ee6894dcc763
Evan Lloyd <eulloyd@gmail.com>
2014-03-27 19:16:34 +0900

    Add current_user events and venues to pages#inside

--- spec/features/creating_events_spec.rb
+++ spec/features/creating_events_spec.rb
@@ -5,10 +5,10 @@ feature "Creating events" do
 		@user = create(:admin_user)
 		@band = create(:band)
 		@venue = create(:venue)
-		sign_in @user
 	end
 
 	scenario "Users can create events" do
+		sign_in @user
 		visit new_event_en_path
 		fill_in "event[name]", with: "Best party ever"
 		fill_in "event[contact]", with: "010 -1234-1234"
@@ -17,5 +17,11 @@ feature "Creating events" do
 		select @venue.name, from: "event[venue_id]"
 		click_button "Create Event"
 		expect(page).to have_content("Event Created!")
+	end
+
+	scenario "Users can see events they created on the sign in page" do
+		@event = create(:event, user: @user)
+		sign_in @user
+		expect(page).to have_content(@event.name)
 	end
 end
--- spec/features/creating_venues_spec.rb
+++ spec/features/creating_venues_spec.rb
@@ -3,9 +3,10 @@ require "spec_helper"
 feature "Creating venues" do
 	before do
 		@user = create(:admin_user)
-		sign_in @user
 	end
+
 	scenario "Users can create venues" do
+		sign_in @user
 		visit new_venue_en_path
 		fill_in "Name", with: "Avalon"
 		fill_in "Phone", with: "010-1234-1234"
@@ -18,5 +19,9 @@ feature "Creating venues" do
 		expect(page).to have_content("Venue Created!")
 	end
 
-	
+	scenario "users can see venues they created on the sign in page" do
+		@venue = create(:venue, user: @user)
+		sign_in @user
+		expect(page).to have_content(@venue.name)
+	end
 end
