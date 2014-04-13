57750cde2ec3d80d855c26dec18da40b86e1fd27
Evan Lloyd <eulloyd@gmail.com>
2014-04-13 13:42:38 +0900

    Update band edit form to use same layout as reg namespace

--- spec/features/admin_bands_panel_spec.rb
+++ spec/features/admin_bands_panel_spec.rb
@@ -14,16 +14,18 @@ feature "Admin band panel" do
 	end
 
 	scenario "admin users can edit bands" do
-		visit admin_bands_path
+		visit admin_bands_en_path
 		click_link @band.name
-		fill_in "Name", with: "psy psy"
-		fill_in "Korean name", with: "asdf"
-		fill_in "Contact", with: "psy@psy.com"
-		fill_in "Facebook", with: "facebook.com/psy"
-		fill_in "Twitter", with: "twitter.com/psy"
-		fill_in "Site", with: "psy.com"
-    check "band[approved]"
-		click_button "Update band"
+		fill_in "band[name]", with: "psy psy"
+		fill_in "band[korean_name]", with: "asdf"
+		fill_in "band[contact]", with: "psy@psy.com"
+		fill_in "band[facebook]", with: "facebook.com/psy"
+		fill_in "band[myspace]", with: "myspace.com/psy"
+		fill_in "band[bandcamp]", with: "bandcamp.com/pys"
+		fill_in "band[twitter]", with: "twitter.com/psy"
+		fill_in "band[site]", with: "psy.com"
+	    check "band[approved]"
+		click_button "Update Band"
 		expect(page).to have_content("updated")
 	end
 end
