476a3bdc16cbfa18d4821ed905616d3b8d01167b
Evan Lloyd <eulloyd@gmail.com>
2014-04-15 18:02:04 +0900

    wtf tests

--- spec/features/slide_spec.rb
+++ spec/features/slide_spec.rb
@@ -4,10 +4,11 @@ feature "Homepage slides" do
 	before do 
 		@slide = create(:slide, active: true)
 	end
+	#broken, slides render fine, js issue?
 	scenario "show active slides" do
 		visit root_path
 	  	expect(page).to have_content @slide.ko_title
-	  	expect(page).to have_content @slide.link
 	  	expect(page).to have_content @slide.ko_description
+	  	expect(page).to have_content @slide.anchor
 	end
 end
