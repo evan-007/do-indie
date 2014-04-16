f54769a88686441bc88f1c4e9d72d5ba1f387e72
Evan Lloyd <eulloyd@gmail.com>
2014-04-16 20:41:14 +0900

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,9 +89,9 @@ DoIndie::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ['S3_BUCKET_NAME'],
+    :access_key_id => ['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ['AWS_SECRET_ACCESS_KEY']
     }
   }
 
