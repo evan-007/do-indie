e393a5b7af4b14989c3e5c45ba1f15343a5cf242
Evan Lloyd <eulloyd@gmail.com>
2014-04-16 21:38:42 +0900

    s3 config again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ DoIndie::Application.configure do
   
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_host_name => 'doindielive.s3.amazonaws.com',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
