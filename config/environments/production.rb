fa2ee0ce0ab605f1535561cffee30ad305f696ec
Evan Lloyd <eulloyd@gmail.com>
2014-04-16 20:43:01 +0900

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,16 +100,16 @@ DoIndie::Application.configure do
   
   # Mailer
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => ENV["DOMAIN"] }
+  config.action_mailer.default_url_options = { :host => ["DOMAIN"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV["SMTP_SERVER"],
+    address: ["SMTP_SERVER"],
     openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
-    port: ENV["SMTP_PORT"],
-    domain: ENV["MAILER_DOMAIN"],
+    port: ["SMTP_PORT"],
+    domain: ["MAILER_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["SMTP_USER"],
-    password: ENV["SMTP_PWD"]
+    user_name: ["SMTP_USER"],
+    password: ["SMTP_PWD"]
   }
 end
