ef3b2049eded2a2b4a1ee40a58041f5a36aa8786
Evan Lloyd <eulloyd@gmail.com>
2014-04-17 17:36:01 +0900

    config typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,8 @@ DoIndie::Application.configure do
   
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_host_name => 'doindielive.s3.amazonaws.com', #no change this  :s3_credentials => {
+  :s3_host_name => 'doindielive.s3.amazonaws.com', #no change this
+    :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
