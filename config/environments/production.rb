fdfb1c7688afa32ef4bc6768857324f5601157a4
Evan Lloyd <eulloyd@gmail.com>
2014-04-17 17:24:53 +0900

    gemfile update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,7 @@ DoIndie::Application.configure do
   
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_host_name => 'doindielive.s3.amazonaws.com',
-  :s3_credentials => {
+  :s3_host_name => 'doindielive.s3.amazonaws.com', #no change this  :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
