79fd67c9bcbef2310a277a42c360f23c0134bddb
Thao Le <thaolt@songphi.com>
2014-04-17 15:54:19 +0000

    seperated will_paginate config file

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -8,20 +8,3 @@ Kaminari.configure do |config|
   # config.page_method_name = :page
   # config.param_name = :page
 end
-
-if defined?(WillPaginate)
-  module WillPaginate
-    module ActiveRecord
-      module RelationMethods
-        def per(value = nil) per_page(value) end
-        def total_count() count end
-        def last_page? 
-          current_page >= total_pages 
-        end
-      end
-    end
-    module CollectionMethods
-      alias_method :num_pages, :total_pages
-    end
-  end
-end
