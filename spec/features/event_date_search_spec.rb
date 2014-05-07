4e6fd5b037642ba60865cf1c230efb6e1358d10b
Evan Lloyd <eulloyd@gmail.com>
2014-05-07 17:19:23 +0900

    Complete date search feature

--- spec/features/event_date_search_spec.rb
+++ spec/features/event_date_search_spec.rb
@@ -7,14 +7,14 @@ feature "Event date search" do
   
   scenario "returns events that match the date" do
     visit events_en_path
-    fill_out "date_search", with: "2014/05/06"
+    fill_in "date_search", with: "2014/05/06"
     click_button "Search"
     expect(page).to have_content(@event.name)
   end
   
   scenario "doesn't return events that don't match query" do
     visit events_en_path
-    fill_out "date_search", with: "2014/04/04"
+    fill_in "date_search", with: "2014/04/04"
     click_button "Search"
     expect(page).to have_content("Sorry, no matches")
   end
