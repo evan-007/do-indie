72356441d7249c497c47ee8836f4097452be1d60
Evan Lloyd <eulloyd@gmail.com>
2014-05-10 19:53:20 +0900

    Add tests for search_model

---
+++ spec/models/search_model_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "SearchModel" do
+	context "when no query is present" do
+		it "returns all instances of a model" 
+	end
+
+	context "when a query is present" do
+		it "returns search results"
+	end
+end
