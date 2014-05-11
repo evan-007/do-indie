062c9b5300a881731332ffaf319976676a48139e
Evan Lloyd <eulloyd@gmail.com>
2014-05-11 09:31:32 +0900

    Add sort to admin#venues

--- app/controllers/admin/venues_controller.rb
+++ app/controllers/admin/venues_controller.rb
@@ -1,4 +1,5 @@
 class Admin::VenuesController < Admin::BaseController
+  helper_method :sort_column, :sort_direction
   before_action :set_venue, only: [
     :show,
     :edit,
@@ -8,7 +9,7 @@ class Admin::VenuesController < Admin::BaseController
 
 
   def index
-    @venues = Venue.order(params[:sort]).admin_search(params[:search], params[:page])
+    @venues = Venue.order(sort_column + " " + sort_direction).admin_search(params[:search], params[:page])
   end
 
   def show
@@ -45,23 +46,31 @@ class Admin::VenuesController < Admin::BaseController
 
   private
 
-  def set_venue
-    @venue = Venue.friendly.find(params[:id])
-  rescue
-    flash[:alert] = "The venue with an id of #{params[:id]} doesn't exist."
-    redirect_to admin_venues_path
-  end
+    def set_venue
+      @venue = Venue.friendly.find(params[:id])
+    rescue
+      flash[:alert] = "The venue with an id of #{params[:id]} doesn't exist."
+      redirect_to admin_venues_path
+    end
 
-  def venue_params
-    params.require(:venue).permit(
-    :name, :korean_name,
-    :avatar, :phone, :address,
-    :en_bio, :ko_bio, :facebook,
-    :twitter, :cafe, :website,
-    :photo, :minimap, :email,
-    :en_directions, :ko_directions,
-    :approved, :city_id
-    )
-  end
+    def venue_params
+      params.require(:venue).permit(
+      :name, :korean_name,
+      :avatar, :phone, :address,
+      :en_bio, :ko_bio, :facebook,
+      :twitter, :cafe, :website,
+      :photo, :minimap, :email,
+      :en_directions, :ko_directions,
+      :approved, :city_id
+      )
+    end
+  
+    def sort_column
+      params[:sort] || "created_at"
+    end
+
+    def sort_direction
+      params[:direction] || "desc"
+    end
 
 end
