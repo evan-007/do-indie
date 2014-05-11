c7e2d41f727a257d5133b6b6fdeaedf71a8eb18c
Evan Lloyd <eulloyd@gmail.com>
2014-05-11 14:49:23 +0900

    Add search to admin managers, add titles to blog index and posts

--- app/models/venue_manager.rb
+++ app/models/venue_manager.rb
@@ -8,8 +8,8 @@ class VenueManager < ActiveRecord::Base
 
   def self.search_and_order(search, page_number)
     if search
-      where("username LIKE ?", "%#{search.downcase}%").order(
-      admin: :desc, username: :asc
+      joins(:user).where("users.username LIKE ?", "%#{search.downcase}%").order(
+      user_id: :desc
       ).page page_number
     else
       order(approved: :asc, user_id: :asc).page page_number
